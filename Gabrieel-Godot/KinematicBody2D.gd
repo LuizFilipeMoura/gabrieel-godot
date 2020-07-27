a617f7b2f3026c39e9f8f07bce22c6c5508dd73a
luiz filipe de moura <luizfilipeholiday@gmail.com>
2020-07-26 22:59:22 -0400

    Movimento e pulo adicionados
    Issue: animacao de pulo com erro

--- Gabrieel-Godot/KinematicBody2D.gd
+++ Gabrieel-Godot/KinematicBody2D.gd
@@ -2,19 +2,42 @@ extends KinematicBody2D
 
 const UP = Vector2(0,-1)
 const GRAVITY = 20
-const ACCELERATION = 5
-const MAX_SPEED = 100
-const JUMP_HEIGHT = -550
+const ACCELERATION = 3
+const MAX_SPEED = 175
+const JUMP_HEIGHT = -350
 
 var motion = Vector2()
 
 func _physics_process(delta):
 	motion.y += GRAVITY
 	
+	var friction = false
+	
 	if Input.is_action_pressed("move_left"):
-		motion.x = -MAX_SPEED
+		motion.x -=  ACCELERATION
+		motion.x = max(motion.x, -MAX_SPEED)
+		$AnimatedSprite.flip_h = true
+		$AnimatedSprite.play("player_running")
+		
 	elif Input.is_action_pressed("move_right"):
-		motion.x = MAX_SPEED
+		motion.x +=  ACCELERATION
+		motion.x = min(motion.x, MAX_SPEED)
+		$AnimatedSprite.flip_h = false
+		$AnimatedSprite.play("player_running")
+	else:
+		friction = true
+		$AnimatedSprite.play("player_idle")
+		
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			motion.y = JUMP_HEIGHT
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.2)
+	else:
+		if motion.y < 0:
+			$AnimatedSprite.play("player_jumping")
+		if friction == true:
+			motion.x = lerp(motion.x, 0, 0.05)
 	motion = move_and_slide(motion, UP)
 
 # Declare member variables here. Examples:
