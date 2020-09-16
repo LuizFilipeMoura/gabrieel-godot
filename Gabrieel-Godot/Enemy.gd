53d688f63f4f0d4bcfd8d1005ca2093271633725
luiz filipe de moura <luizfilipeholiday@gmail.com>
2020-09-16 12:46:16 -0400

    Iluminacao global adicionada

--- Gabrieel-Godot/Enemy.gd
+++ Gabrieel-Godot/Enemy.gd
@@ -18,11 +18,18 @@ func die():
 	isDead = true
 	$AnimatedSprite.play("enemy_death")
 	yield($AnimatedSprite, "animation_finished")
-	$CollisionShape2D.scale = Vector2(2, 0.4)
-	$CollisionShape2D.position = Vector2(0, 10)
+	$CollisionShape2D.scale = Vector2(2, 0.2)
+	$CollisionShape2D.transform.origin = Vector2(7, 20)
 		
-func _on_PlayerDetector_body_entered(body):
+func _on_Head_body_entered(body):
+	
 	if(body.name == 'Player'):
 		if(!isDead):
 			die()
 
+
+
+func _on_Body_body_entered(body):
+	if(body.name == 'Player' && !isDead):
+			get_tree().change_scene("res://Node2D.tscn")
+	pass # Replace with function body.
