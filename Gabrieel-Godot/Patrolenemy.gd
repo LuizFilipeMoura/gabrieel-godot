1b5acf0ec4415b9d3e386be497c4cef205aa57d6
Minerororu <pedro.dx.2008@gmail.com>
2020-10-12 14:28:55 -0300

    Patrolenemy adicionado
    Adicionei um inimigo q anda

---
+++ Gabrieel-Godot/Patrolenemy.gd
@@ -0,0 +1,128 @@
+extends KinematicBody2D
+
+const UP = Vector2(0,-1)
+const GRAVITY = 10
+var ACCELERATION = 2
+var MAX_SPEED = 300
+var motion = Vector2()
+var isReadyToMove = false
+var isMovingto = "right"
+var lookingTime = 2
+var isDead = false;
+var life = 2
+var damage = 2
+var isPilot = false
+signal hurtTank
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	isReadyToMove = true
+	$AnimatedSprite.play("enemy_idle")
+	if self.name == "Enemy2":
+		life = 4 
+
+func stop():
+	$AnimatedSprite.play("enemy_idle")
+	isReadyToMove = false
+
+
+
+func  _on_Timer_timeout():
+	if !isDead:
+		isReadyToMove = true
+	
+func _process(delta):
+	#motion.y += GRAVITY
+	move_and_collide(Vector2(0,10))
+	if !isReadyToMove:
+		motion.x = lerp(motion.x, 0, 0.4)
+	if(isReadyToMove):
+		if(motion.x > 6):
+			$AnimatedSprite.flip_h = false
+			$AnimatedSprite.play("Enemy_Walk")
+		elif (motion.x < -6):
+			$AnimatedSprite.flip_h = true
+			$AnimatedSprite.play("Enemy_Walk")
+		else:
+			$AnimatedSprite.play("enemy_idle")
+		if isMovingto == "left":
+			motion.x -=  ACCELERATION # LEFT
+			motion.x = max(motion.x, -MAX_SPEED)
+		if isMovingto == "right":
+			motion.x +=  ACCELERATION # RIGHT
+			motion.x = min(motion.x, MAX_SPEED)
+	motion = move_and_slide(motion, UP)
+#	pass
+
+
+
+
+func _physics_process(delta):
+	if !isDead && !is_on_floor() && !isPilot:
+		move_and_collide(Vector2(0, 10))
+
+func die():
+	isDead = true
+	stop()
+	$AnimatedSprite.play("enemy_death")
+	yield($AnimatedSprite, "animation_finished")
+	self.queue_free()
+		
+	
+		
+func _on_Head_body_entered(body):
+	if(body.name == 'Player'):
+		if(isPilot):
+			body.smallJump()
+			emit_signal("hurtTank")
+			$AnimatedSprite.play("enemy_death")
+			yield($AnimatedSprite, "animation_finished")
+			$AnimatedSprite.play("enemy_idle")
+		elif(!isDead):
+			body.smallJump()
+			die()
+			
+
+func hurt(damageTaken):
+	
+	if(!isDead):
+		$AnimatedSprite.play("enemy_hurt")
+		yield($AnimatedSprite, "animation_finished")
+		$AnimatedSprite.play("enemy_idle")
+	life = life - damageTaken
+	if(life <= 0):
+		die()
+
+func _on_Body_body_entered(body):
+	if(body.name == 'Player' && !isDead):
+		if(body.position.x - position.x):
+			$AnimatedSprite.flip_h = true
+		else:
+			$AnimatedSprite.flip_h = false
+		body.hurt(damage)
+		
+func isPilot():
+	isPilot = true
+	$CollisionShape2D.disabled = true
+
+
+
+	pass 
+
+
+
+
+
+func _on_TurnPointR_body_entered(body):
+	if(body.is_in_group("Enemy")):
+		stop()
+		$Timer.start(lookingTime)
+		isMovingto = "left"
+
+
+
+func _on_TurnPointL_body_entered(body):
+	if(body.is_in_group("Enemy")):
+		stop()
+		$Timer.start(lookingTime)
+		isMovingto = "right"
